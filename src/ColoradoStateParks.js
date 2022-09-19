67869f3a8c357ed1a0991c5551a26b9c31947bb3
Sugut <sugutvictorine@gmail.com>
2022-09-19 11:41:53 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,5 +1,7 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
